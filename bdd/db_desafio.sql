4475d3b90a242bb48ada78bc2c0cd015607604e4
fitty0930 <martinaguirre08@gmail.com>
2022-05-11 16:10:17 -0300

    agregada db

--- bdd/db_desafio.sql
+++ bdd/db_desafio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-05-2022 a las 04:28:22
+-- Tiempo de generación: 11-05-2022 a las 21:09:29
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 7.4.29
 
@@ -32,6 +32,20 @@ CREATE TABLE `roles` (
   `nombre` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id_rol`, `nombre`) VALUES
+(1, 'Anunciante'),
+(3, 'Publicista'),
+(4, 'Vendedor'),
+(5, 'RRHH'),
+(8, 'Administrador'),
+(9, 'Gerente'),
+(11, 'Dueño'),
+(13, 'Sistemas');
+
 -- --------------------------------------------------------
 
 --
@@ -51,11 +65,43 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `password`, `admin`) VALUES
 (1, 'martin0930', '12345', 1),
-(14, '12345', '12345', 0);
+(14, 'joseluisperales', '12345', 0),
+(17, 'hola', 'hola', 0),
+(20, 'ricardoarjona', '12345', 0),
+(21, 'webexport', 'webexport', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarioxrol`
+--
+
+CREATE TABLE `usuarioxrol` (
+  `id_userxrol` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_rol` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarioxrol`
+--
+
+INSERT INTO `usuarioxrol` (`id_userxrol`, `id_usuario`, `id_rol`) VALUES
+(7, 1, 1),
+(8, 1, 3),
+(9, 1, 5),
+(11, 14, 13),
+(12, 21, 13);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_rol`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -64,14 +110,46 @@ ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id_usuario`);
 
 --
+-- Indices de la tabla `usuarioxrol`
+--
+ALTER TABLE `usuarioxrol`
+  ADD PRIMARY KEY (`id_userxrol`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_rol` (`id_rol`),
+  ADD KEY `id_usuario_2` (`id_usuario`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarioxrol`
+--
+ALTER TABLE `usuarioxrol`
+  MODIFY `id_userxrol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `usuarioxrol`
+--
+ALTER TABLE `usuarioxrol`
+  ADD CONSTRAINT `usuarioxrol_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `usuarioxrol_ibfk_2` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
